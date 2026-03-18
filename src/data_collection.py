c65967ed53d3bf262aa0a154a18c6d45c2ed3243
luizcaixeta <luizfernandosusu@gmail.com>
2026-03-18 05:59:18 -0300

    fix path

--- src/data_collection.py
+++ src/data_collection.py
@@ -7,19 +7,21 @@ def data_collection():
     api = KaggleApi()
     api.authenticate()
 
-    os.makedirs("../data", exist_ok=True)
+    path = "../data/raw"
+
+    os.makedirs(path , exist_ok=True)
 
     api.competition_download_files(
         "house-prices-advanced-regression-techniques",
-        path="../data",
+        path=path,
     )
 
-    for f in os.listdir("../data/raw"):
+    for f in os.listdir(path):
         if f.endswith(".zip"):
-            zip_path = os.path.join("../data", f)
+            zip_path = os.path.join(path, f)
 
             with zipfile.ZipFile(zip_path, "r") as z:
-                z.extractall("../data")
+                z.extractall(path)
 
             os.remove(zip_path)
             print(f"Descompactado: {f}")
