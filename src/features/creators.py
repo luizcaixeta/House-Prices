f77d9d011426b83bbce96880f34b5adba6758ee7
luizcaixeta <luizfernandosusu@gmail.com>
2026-04-10 15:34:56 -0300

    feat(features): add creators module for custom feature generation

--- src/features/creators.py
+++ src/features/creators.py
@@ -1,36 +1,37 @@
-import numpy as np 
 from sklearn.base import BaseEstimator, TransformerMixin
 
 class FeatureCreator(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
-        return self 
-    
+        return self
+
     def transform(self, X):
         X = X.copy()
 
-        X['TotalBathrooms'] = (
-            X['FullBath']
-            + 0.5 * X['HalfBath']
-            + X['BsmtFullBath']
-            + 0.5 * X['BsmtHalfBath']
+        X["TotalBathrooms"] = (
+            X["FullBath"]
+            + 0.5 * X["HalfBath"]
+            + X["BsmtFullBath"]
+            + 0.5 * X["BsmtHalfBath"]
         )
 
-        X['Remod'] = (X['YearBuilt'] != X['YearRemodAdd']).astype(int)
+        X["Remod"] = (X["YearBuilt"] != X["YearRemodAdd"]).astype(int)
+        X["Age"] = X["YrSold"] - X["YearRemodAdd"]
 
-        X['Age'] = X['YrSold'] - X['YearRemodAdd']
+        return X
 
-        return X 
-    
+
 class BinarizeFeatures(BaseEstimator, TransformerMixin):
+    def __init__(self, pool_categories, misc_feature_categories):
+        self.pool_categories = tuple(pool_categories)
+        self.misc_feature_categories = tuple(misc_feature_categories)
+
     def fit(self, X, y=None):
-        return self 
-    
+        return self
+
     def transform(self, X):
         X = X.copy()
 
-        X['has_pool'] = X['PoolQC'].isin(['Ex', 'Fa', 'Gd']).astype(int)
-        X['has_misc_feature'] = X['MiscFeature'].isin(['Shed', 'Gar2', 'Othr', 'TenC']).astype(int)
+        X["has_pool"] = X["PoolQC"].isin(self.pool_categories).astype(int)
+        X["has_misc_feature"] = X["MiscFeature"].isin(self.misc_feature_categories).astype(int)
 
         return X
-    
-    
