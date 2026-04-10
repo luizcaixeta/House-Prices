b742583c2f24dd7540cea2767683edd28c951100
luizcaixeta <luizfernandosusu@gmail.com>
2026-04-10 15:35:21 -0300

    feat(features): add encoders for feature encoding

--- src/features/encoders.py
+++ src/features/encoders.py
@@ -1,4 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd 
 
 class BaseDFTransformer(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
@@ -7,20 +8,40 @@ class BaseDFTransformer(BaseEstimator, TransformerMixin):
 class BinaryEncoder(BaseDFTransformer):
     def __init__(self, mappings):
         self.mappings = mappings 
-
+    
     def transform(self, X):
         X = X.copy()
+
         for col, mapping in self.mappings.items():
             if col in X.columns:
                 X[col] = X[col].map(mapping)
-        return X
-        
+                X[col] = pd.to_numeric(X[col], errors='coerce').fillna(0)
+        return X 
+    
 class OrdinalEncoderCustom(BaseDFTransformer):
     def __init__(self, mappings):
         self.mappings = mappings 
 
     def transform(self, X):
+        X = X.copy()
+
         for col, mapping in self.mappings.items():
             if col in X.columns:
                 X[col] = X[col].map(mapping)
+                X[col] = pd.to_numeric(X[col], errors='coerce').fillna(0)
+
+        return X
+    
+class FoundationEncoder(BaseDFTransformer):
+    def __init__(self, mapping, column="Foundation", default_value=0):
+        self.mapping = mapping
+        self.column = column
+        self.default_value = default_value
+
+    def transform(self, X):
+        X = X.copy()
+
+        if self.column in X.columns:
+            X[self.column] = X[self.column].map(self.mapping).fillna(self.default_value)
+
         return X
